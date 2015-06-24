b7d109a5fe7af4f8847de04791da87e53dc53012
John Hampton <john_j_hampton@yahoo.com>
2015-06-24 17:22:52 -0400

    Log the 'latest track of a group' per Soundcloud API demo to console in route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.RSVP.hash({
+      track: SC.get("/groups/55517/tracks", {limit: 1}, function(tracks){
+        console.log(tracks);
+        return tracks;
+      })
+    });
+  }
 });
